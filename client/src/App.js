817db3632f245560ba867e04def50aa73f543072
ABHIJITH TA <90981851+AbhijithTA@users.noreply.github.com>
2024-03-07 10:53:18 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import EmailCompose from "./components/Email Compose/EmailCompose";
 import EmailDash from "./components/Email Dashboard/EmailDash";
 import EmailDraft from "./components/Email Draft/EmailDraft";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import EditEmail from "./components/Email Edit/EmailEdit";
+
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={<EmailDash />} />
           <Route path="/compose" element={<EmailCompose />} />
           <Route path="/draft" element={<EmailDraft />} />
-          <Route path="/edit-email/:index" element={<EditEmail />} />
+          
         </Routes>
       </Router>
     </>
